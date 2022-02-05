6761aa8eef4f7bd14cc4418169bbdf88448ba4c7
SobranDM <sobran@gmail.com>
2022-02-05 13:12:51 -0800

    error handlers setup

--- src/app.js
+++ src/app.js
@@ -2,6 +2,17 @@ if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
 
+// Router files
+const notFound = require("./errors/notFound");
+const errorHandler = require("./errors/errorHandler");
 
+app.use(express.json());
+
+// App.use Route Handlers
+
+
+// App.use Routes
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
