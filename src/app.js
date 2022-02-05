d6de67a6d1bec34b494ee0535752d68c64857411
SobranDM <sobran@gmail.com>
2022-02-05 13:20:02 -0800

    app.js done

--- src/app.js
+++ src/app.js
@@ -3,13 +3,18 @@ const express = require("express");
 const app = express();
 
 // Router files
+const moviesRouter = require("./movies/movies.router");
+const theatersRouter = require("./theaters/theaters.router");
+const reviewsRouter = require("./reviews/reviews.router");
 const notFound = require("./errors/notFound");
 const errorHandler = require("./errors/errorHandler");
 
 app.use(express.json());
 
 // App.use Route Handlers
-
+app.use("/movies", moviesRouter);
+app.use("/reviews", reviewsRouter);
+app.use("/theaters", theatersRouter);
 
 // App.use Routes
 app.use(notFound);
